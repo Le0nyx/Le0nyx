e5d8ce02393da946c03c0a1052d34293f0134b6c
Leon A <130183414+Le0nyx@users.noreply.github.com>
2024-02-13 15:24:45 +0100

    added main scripts from first unity game (very unpolished)

---
+++ personal/first unity project/player1attack.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player1attack : MonoBehaviour
+{
+
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    public Transform attackPos;
+    public float attackRange;
+    public LayerMask whatIsEnemies;
+    public int damage;
+    public Animator playerAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBtwAttack <= 0)
+        {
+            if (Input.GetKey(KeyCode.G))
+            { 
+                playerAnim.SetTrigger("Attack");
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Player2>().TakeDamage(damage);
+                }
+            }
+
+            timeBtwAttack = startTimeBtwAttack;
+        }
+        else
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }
+
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+}
